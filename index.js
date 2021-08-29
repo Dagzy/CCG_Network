6a3cf72a9aba0d7ee451aed9d43e62845a69283d
Quincy <quincye.williams@gmail.com>
2021-08-29 00:11:04 -0400

    quick push

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-
+const PORT = 9001;
+const app = express();
 dotenv.config();
-
-
-
-
-
-
-app.listen(9001, ()=>{
-    console.log("IT'S OVER 9000!");
+mongoose.connect(dotenv.MONGO_URL ||"mongodb://localhost:27017/CCG_Network", (err, db)=>{
+    console.log(err, db); 
 })
+app.get("/", (req, res) => {
+    res.send("<h1>Hello!</hello>");
+});
+
+app.listen(PORT, LEVEL => {
+    console.log(`That's over 9000!`);
+});
