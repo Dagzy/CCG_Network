705e0f24201b5aadd68945b45cee514acadc91ad
Quincy <quincye.williams@gmail.com>
2021-09-15 03:49:58 -0400

    tweaked routes.js pretty hard. Lots to do, but feel like some landscaping might get it off the ground.

--- routes/posts.js
+++ routes/posts.js
@@ -10,9 +10,9 @@ router.post("/", (req, res) => {
   const { body } = req;
   const p = new Post(body);
   p.save()
-    .then((data) => {
-      console.log(data);
-      res.status(200).json({ message: "Post Created!", postId:data._id });
+    .then( post => {
+      console.log(post);
+      res.status(200).json({ message: "Post Created!", postId:post._id });
     })
     .catch((err) => {
       console.log(err);
@@ -23,55 +23,85 @@ router.post("/", (req, res) => {
 router.put("/:id", (req, res) => {
   const { id } = req.params;
   const { owner, content, img } = req.body;
+  const requestingUser = mongoose.Types.ObjectId(owner).toString();
   //JWT auth will go here
-  Post.findById(id).then((post) => {
-    if (post.enabled) {
-      if (post.owner.toString() === mongoose.Types.ObjectId(owner).toString()) {
-        Post.findByIdAndUpdate(
-          id,
-          { $set: { content: content, img: img } },
-          { new: true }
-        ).then((data) => {
-          res.status(200).json({ message: "You Updated A Post!" });
-        });
+  User.findById(requestingUser).then(user => {
+    const { isAdmin: adminIsUpdatingPost } = user;
+    Post.findById(id).then( foundPost => {
+      const { enabled: postIsEnabled, owner } = foundPost;
+      const postOwner = owner.toString();
+      const validUserIsUpdatingPost = postOwner === requestingUser;
+      if (postIsEnabled) {
+        if (validUserIsUpdatingPost || adminIsUpdatingPost) {
+          Post.findByIdAndUpdate(
+            id,
+            { $set: { content: content, img: img } },
+            { new: true }
+          ).then(updatedPost => {
+            res.status(200).json({ message: "You Updated A Post!", updatedPost: updatedPost });
+          });
+        } else {
+          res.status(403).json({ message: "That action cannot be completed." });
+        }
       } else {
-        res.status(403).json({ message: "You can't perform that action." });
+        res.json({ message: "Post not found." });
       }
-    } else {
-      res.json({ message: "Post not found." });
-    }
-  });
+    });
+  })
 });
 router.get("/", (req, res) => {});
 //get timeline
 router.get("/", (req, res) => {});
 //like a post
-router.put("/:id", (req, res) => {});
+router.put("/like/:id", (req, res) => {
+  const { id } = req.params;
+  const {owner: requestingUser} = req.body;
+  //JWT stuff needs to go here
+  Post.findById(id).then(post => {
+    const { likes, _id } = post;
+    if(likes.includes(requestingUser)){
+      Post.findByIdAndUpdate(id, {$pull:{likes:requestingUser}}, {new:true}).then(unlikedPost => {
+        console.log(unlikedPost);
+        res.status(200).json({message:"UNLIKING"})
+      })
+    }else{
+      console.log(post);
+      Post.findByIdAndUpdate(_id, {$push:{likes:requestingUser}}, {new:true}).then(likedPost=> {
+        console.log(likedPost);
+        res.status(200).json({message:"LIKING"})
+      })
+    }
+  })
+
+});
 //(DISABLES) a post
 router.delete("/:id", (req, res) => {
   const { body } = req;
-  const { userId } = body;
+  const { owner } = body;
   const { id } = req.params;
   //jwt auth will go here
-  User.findById(userId).then(user => {
-    Post.findById(id).then((post) => {
-      console.log(user, post);
-      if (post.owner.toString() === user._id.toString() || user.isAdmin) {
-        console.log(post.owner.toString(), user._id.toString());
-        if (post.enabled) {
+  const requestingUser = mongoose.Types.ObjectId(owner).toString();
+  User.findById(requestingUser).then(user => {
+    const {isAdmin: adminIsDeletingPost} = user;
+    Post.findById(id).then(post => {
+      const {owner, enabled} = post;
+      const postOwner = owner.toString();
+      const validUserIsDeletingPost = postOwner === requestingUser;
+      if (validUserIsDeletingPost || adminIsDeletingPost) {
+        if (enabled) {
           Post.findByIdAndUpdate(id, { $set: { enabled: false } })
-            .then((data) => {
+            .then(data => {
               res.status(200).json({ message: "Post successfully deleted." });
             })
-            .catch((err) => {
+            .catch(err => {
               console.log(err);
               res.status(500).json("An error occurred.");
             });
         } else {
-          res.send("Post Not Found.");
+          res.status(401).json({ message:"Post Not Found." });
         }
       } else {
-        res.status(403).json({ message: "You can't perform that action." });
+        res.status(403).json({ message: "That action cannot be completed." });
       }
     });
   })
